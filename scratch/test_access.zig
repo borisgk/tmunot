d84a1f91844f2746e23dd000081ae61d459096b8
Boris Kimel <borisk@rus9n.com>
2026-06-15 20:33:43 +0300

    chore: remove scratch files and temporary test scripts

--- scratch/test_access.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-pub fn main() !void {
-    std.posix.access("scratch", std.posix.F_OK) catch |err| {
-        std.debug.print("Error: {}\n", .{err});
-        return;
-    };
-    std.debug.print("Exists!\n", .{});
-}
